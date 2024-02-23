8578c086c7bb76ffe34aa9af4901393d57ee23a4
Samdasoo1076 <dlwlalsdkd@gmail.com>
2024-02-23 16:29:14 +0900

    add

---
+++ API_JS/api_key.js
@@ -0,0 +1,5 @@
+const config = {
+    api_key1: "jGCW2cH1wW9+FUZW43rv/C8jBSX0Jqsa0NgA37UvJcc55KRrgFpYbq6BrUzvQ/YQclbVZjcKOP5qMYeoCbSoJw==",
+    api_url1:"https://api.odcloud.kr/api/15052392/v1/uddi:77246885-358a-4389-8338-d4c5df6be1f8_201908291426?page=1&perPage=10&serviceKey=jGCW2cH1wW9%2BFUZW43rv%2FC8jBSX0Jqsa0NgA37UvJcc55KRrgFpYbq6BrUzvQ%2FYQclbVZjcKOP5qMYeoCbSoJw%3D%3D",
+
+};
